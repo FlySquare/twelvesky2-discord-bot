b6d066c5bb6e7cf2a0fd60686be9840125750dc4
Umut Can Arda <umutkonurinso@gmail.com>
2022-12-18 23:40:22 +0300

    added war message to bot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, GatewayIntentBits, Partials, ActivityType , Collection} = require("discord.js");
+const {Client, GatewayIntentBits, Partials, ActivityType, Collection} = require("discord.js");
 const INTENTS = Object.values(GatewayIntentBits);
 const PARTIALS = Object.values(Partials);
 const client = new Client({
@@ -12,15 +12,13 @@ const client = new Client({
 const fs = require('fs');
 const warChannelId = '1054060249985863840';
 const warTimeFileName = 'C:/warTime.txt';
-
+const readline = require('readline');
 global.client = client;
 client.commands = (global.commands = []);
-
 const {readdirSync} = require("fs")
 const {TOKEN} = require("./config.json");
 readdirSync('./commands').forEach(f => {
     if (!f.endsWith(".js")) return;
-
     const props = require(`./commands/${f}`);
     client.commands.push({
         name: props.name.toLowerCase(),
@@ -40,24 +38,48 @@ readdirSync('./events').forEach(e => {
     console.log(`[EVENT] ${name} loaded as event.`)
 });
 client.login(TOKEN).then(app => {
-    console.log(`[BOT] Token is verified.`)
     client.user.setPresence({
-        activities: [{ name: `lunaticsky2.online`, type: ActivityType.Playing, url:'https://lunaticsky2.online' }],
+        activities: [{name: `lunaticsky2.online`, type: ActivityType.Playing, url: 'https://lunaticsky2.online'}],
         status: 'Online',
     });
-    setInterval(()=>{
+    setInterval(() => {
         if (fs.existsSync(warTimeFileName)) {
-            client.channels.fetch(warChannelId).then((channel) => {
-                channel.send(`@everyone |
-                 [:flag_us:] War will start in 10 min! 
-                 [:flag_tr:] Savaş 10 dakika içerisinde başlayacak!
-                 [:flag_mc:] Pertempuran akan dimulai dalam 10 menit!
-                 [:flag_th: ] การต่อสู้จะเริ่มในอีก 10 นาที! 
-                 `);
-            });
-            fs.unlinkSync(warTimeFileName);
+            processLineByLine();
         }
-    },2000);
+    }, 2000);
+    console.log(`[BOT] Token is verified.`)
 }).catch(app => {
     console.log(`[BOT] Token is not verified.`)
 })
+
+
+async function processLineByLine() {
+    const fileStream = fs.createReadStream(warTimeFileName);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+    for await (const line of rl) {
+        if (line.indexOf("[[295]]") != "-1") {
+            if (line.indexOf("(10)") != "-1") {
+                sendMessage(10);
+            } else if (line.indexOf("(5)") != "-1") {
+                sendMessage(5);
+            } else if (line.indexOf("(1)") != "-1") {
+                sendMessage(1);
+            }
+        }
+    }
+}
+
+async function sendMessage(minute = 10) {
+    client.channels.fetch(warChannelId).then((channel) => {
+        channel.send(`@everyone |
+                 [:flag_us:] War will start in ${minute} min!
+                 [:flag_tr:] Savaş ${minute} dakika içerisinde başlayacak!
+                 [:flag_mc:] Pertempuran akan dimulai dalam ${minute} menit!
+                 [:flag_th: ] การต่อสู้จะเริ่มในอีก ${minute} นาที!
+                 `);
+    });
+    fs.unlinkSync(warTimeFileName);
+}
